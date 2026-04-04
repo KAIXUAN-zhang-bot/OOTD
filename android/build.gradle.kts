6376bf44572da87601853adf682d3abea3ba4a94
User <user@example.com>
2026-04-05 01:07:21 +0800

    Add Hilt plugin

--- android/build.gradle.kts
+++ android/build.gradle.kts
@@ -3,4 +3,5 @@ plugins {
     id("com.android.library") version "8.3.0" apply false
     kotlin("android") version "2.0.0" apply false
     kotlin("plugin.serialization") version "2.0.0" apply false
+    id("com.google.dagger.hilt.android") version "2.50" apply false
 }
